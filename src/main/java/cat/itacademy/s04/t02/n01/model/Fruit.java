2ef8634fe612e5eb18fb466c07eb8300e978c07f
resikrys <resicris@gmail.com>
2025-09-19 12:54:29 +0200

    add Fruit entity model

---
+++ src/main/java/cat/itacademy/s04/t02/n01/model/Fruit.java
@@ -0,0 +1,61 @@
+package cat.itacademy.s04.t02.n01.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+//import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fruit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantityKilos;
+
+    //Empty constructor --> mandatory with JPA
+    public Fruit() {
+    }
+
+    public Fruit(int id, String name, int quantityKilos) {
+        this.id = id;
+        this.name = name;
+        this.quantityKilos = quantityKilos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantityKilos() {
+        return quantityKilos;
+    }
+
+    public void setQuantityKilos(int quantityKilos) {
+        this.quantityKilos = quantityKilos;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantityKilos=" + quantityKilos +
+                '}';
+    }
+}
