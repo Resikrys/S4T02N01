980ea0cba844006b08460d761e5f25ed411a1a2f
resikrys <resicris@gmail.com>
2025-09-19 13:51:05 +0200

    upd refactor FruitController hhtp status if-else method

--- src/main/java/cat/itacademy/s04/t02/n01/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/t02/n01/controllers/FruitController.java
@@ -5,7 +5,6 @@ import cat.itacademy.s04.t02.n01.services.FruitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,11 +25,10 @@ public class FruitController {
     @GetMapping("/{id}")
     public ResponseEntity<Fruit> getFruitById(@PathVariable int id) {
         Fruit fruit = fruitService.getFruitById(id);
-        if (fruit != null) {
-            return new ResponseEntity<>(fruit, HttpStatus.OK);
-        } else {
+        if (fruit == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(fruit, HttpStatus.OK);
     }
 
     @GetMapping
@@ -42,23 +40,21 @@ public class FruitController {
     @PutMapping("/{id}")
     public ResponseEntity<Fruit> updateFruit(@PathVariable int id, @RequestBody Fruit fruit) {
         Fruit existingFruit = fruitService.getFruitById(id);
-        if (existingFruit != null) {
-            Fruit updatedFruit = fruitService.updateFruit(id, fruit);
-            return new ResponseEntity<>(updatedFruit, HttpStatus.OK);
-        } else {
+        if (existingFruit == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        Fruit updatedFruit = fruitService.updateFruit(id, fruit);
+        return new ResponseEntity<>(updatedFruit, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFruit(@PathVariable int id) {
         Fruit fruit = fruitService.getFruitById(id);
-        if (fruit != null) {
-            fruitService.deleteFruit(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204 No Content
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404 Not Found
+        if (fruit == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        fruitService.deleteFruit(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
